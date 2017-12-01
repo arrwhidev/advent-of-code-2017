e7e33e6c59b50d5a43f5cd97ae8dd6806fbc7143
arran <arrwhidev@gmail.com>
2017-12-01 23:59:17 +0000

    Added part two

--- 1/1.js
+++ 1/1.js
@@ -1,22 +1,39 @@
 /**
  * Advent of Code 2017 - Day 1
+ *
+ * Captcha 1:
  * 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.
  * 1111 produces 4 because each digit (all 1) matches the next.
  * 1234 produces 0 because no digit matches the next.
  * 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
+ *
+ * Captcha 2:
+ * 1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.
+ * 1221 produces 0, because every comparison is between a 1 and a 2.
+ * 123425 produces 4, because both 2s match each other, but no other digit has a match.
+ * 123123 produces 12.
+ * 12131415 produces 4.
  */
 
-function captcha(input) {
-    const str = input.toString()
-    let result = 0
-    for (let i = 0; i < str.length; i++) {
-        const currNum = str[i]
-        const nextNum = ((str.length - 1) === i) ? str[0] : str[i + 1]
-        if (currNum === nextNum) {
-            result += parseInt(currNum)
-        }
-    }
-    return result;
+ function sumValuesWithCirculaDelta(input, delta) {
+     const str = input.toString()
+     let result = 0
+     for (let i = 0; i < str.length; i++) {
+         const currNum = str[i]
+         const nextNum = str[(i + delta) % str.length]
+         if (currNum === nextNum) {
+             result += parseInt(currNum)
+         }
+     }
+     return result;
+ }
+
+function captcha1(input) {
+    return sumValuesWithCirculaDelta(input, 1)
 }
 
-module.exports = captcha
+function captcha2(input) {
+    return sumValuesWithCirculaDelta(input, input.toString().length / 2)
+}
+
+module.exports = { captcha1, captcha2 }
--- 1/1.spec.js
+++ 1/1.spec.js
@@ -1,15 +1,34 @@
 const { expect } = require('chai')
-const day1 = require('./1')
+const {
+    captcha1,
+    captcha2
+} = require('./1')
 
 describe('Day 1', () => {
-    [
-        { input: 1122, expected: 3 },
-        { input: 1111, expected: 4 },
-        { input: 1234, expected: 0 },
-        { input: 91212129, expected: 9 }
-    ].forEach(test => {
-        it('should sum numbers that are successive', () => {
-            expect(day1(test.input)).to.equal(test.expected)
+    describe('Captcha 1', () => {
+        [
+            { input: 1122, expected: 3 },
+            { input: 1111, expected: 4 },
+            { input: 1234, expected: 0 },
+            { input: 91212129, expected: 9 }
+        ].forEach(test => {
+            it('should sum numbers that are successive', () => {
+                expect(captcha1(test.input)).to.equal(test.expected)
+            })
+        })
+    })
+
+    describe('Captcha 2', () => {
+        [
+            { input: 1212, expected: 6 },
+            { input: 1221, expected: 0 },
+            { input: 123425, expected: 4 },
+            { input: 123123, expected: 12 },
+            { input: 12131415, expected: 4 }
+        ].forEach(test => {
+            it('should sum numbers that match halfway around the input', () => {
+                expect(captcha2(test.input)).to.equal(test.expected)
+            })
         })
     })
 })
