191a523eccb3709906fc74158be69ea6df204024
arran <arrwhidev@gmail.com>
2017-12-07 00:21:31 +0000

    Added day 4 part 2

--- 4/4.js
+++ 4/4.js
@@ -1,4 +1,4 @@
-function passphraseValidator (passphrase) {
+function passphraseValidator(passphrase) {
     const set = new Set()
     return passphrase.split(/\s/g).reduce((isValid, item) => {
         if (!isValid || set.has(item)) return false;
@@ -7,4 +7,27 @@ function passphraseValidator (passphrase) {
     }, true)
 }
 
-module.exports = { passphraseValidator }
+function countDistinctChars(dic, c) {
+    return Object.assign(dic, { [c]: (dic[c] || 0) + 1 })
+}
+
+function isAnagram(word1, word2) {
+    if (word1.length !== word2.length) return false
+    const d1 = word1.split('').reduce(countDistinctChars, {})
+    const d2 = word2.split('').reduce(countDistinctChars, {})
+    return Object.keys(d1).reduce((isMatch, k) => isMatch | (d1[k] != d2[k]), false) === 0
+}
+
+function anagramPassphraseValidator(passphrase) {
+    let isValid = true
+    const words = passphrase.split(/\s/g)
+    for(let i = 0; i < words.length; i++) {
+        for(let j = 0; j < words.length; j++) {
+            if (i === j) continue
+            if (isAnagram(words[i], words[j])) isValid = false
+        }
+    }
+    return isValid
+}
+
+module.exports = { passphraseValidator, isAnagram, anagramPassphraseValidator }
--- 4/4.spec.js
+++ 4/4.spec.js
@@ -1,7 +1,9 @@
 const { expect } = require('chai')
 const fs = require('fs')
 const {
-    passphraseValidator
+    passphraseValidator,
+    anagramPassphraseValidator,
+    isAnagram
 } = require('./4')
 
 describe('Day 4', () => {
@@ -18,7 +20,34 @@ describe('Day 4', () => {
                     .split('\n')
                     .filter(line => line !== '')
                     .reduce((totalValid, line) => (passphraseValidator(line)) ? totalValid + 1 : totalValid, 0)
-                    
+                console.log('total valid passphrases:', total);
+            });
+        })
+    })
+
+    describe('Passphrase challenge 2', () => {
+        it('should return true when words are anagrams', () => {
+            expect(isAnagram('hello', 'world')).to.be.false
+            expect(isAnagram('q', 'qq')).to.be.false
+            expect(isAnagram('hello', 'llheo')).to.be.true
+            expect(isAnagram('iiii', 'oiii')).to.be.false
+            expect(isAnagram('oiii', 'oooi')).to.be.false
+        })
+
+        it('should return true when no phrases are anagrams', () => {
+            expect(anagramPassphraseValidator('abcde fghij')).to.be.true
+            expect(anagramPassphraseValidator('abcde ecdab')).to.be.false
+            expect(anagramPassphraseValidator('a ab abc abd abf abj')).to.be.true
+            expect(anagramPassphraseValidator('iiii oiii ooii oooi oooo')).to.be.true
+            expect(anagramPassphraseValidator('oiii ioii iioi iiio')).to.be.false
+        })
+
+        it('should count number of valid passphrases', () => {
+            fs.readFile('./4/passphrases.txt', 'utf8', (err, contents) => {
+                const total = contents
+                    .split('\n')
+                    .filter(line => line !== '')
+                    .reduce((totalValid, line) => (anagramPassphraseValidator(line)) ? totalValid + 1 : totalValid, 0)
                 console.log('total valid passphrases:', total);
             });
         })
