7d4118225675e5d3e7aff807e779d154998cca79
Arran White <arrwhidev@gmail.com>
2017-12-09 10:46:36 +0000

    Day 5 part 2 complete

--- 5/5.js
+++ 5/5.js
@@ -15,4 +15,26 @@ function maze(input) {
     return count
 }
 
-module.exports = { maze }
+function maze2(input) {
+    const steps = input.split(/\s/g)
+    let offset = 0, count = 0, escaped = false
+    while (!escaped) {
+        const newoffset = offset + parseInt(steps[offset])
+        if(parseInt(steps[offset]) >= 3) {
+            steps[offset]--
+        } else {
+            steps[offset]++
+        }
+
+        offset = newoffset
+
+        if (offset === steps.length || offset < 0) {
+            escaped = true
+        }
+        count++
+    }
+
+    return count
+}
+
+module.exports = { maze, maze2 }
--- 5/5.spec.js
+++ 5/5.spec.js
@@ -1,7 +1,8 @@
 const { expect } = require('chai')
 const fs = require('fs')
 const {
-    maze
+    maze,
+    maze2
 } = require('./5')
 
 describe('Day 5', () => {
@@ -18,6 +19,23 @@ describe('Day 5', () => {
                     .join(' ')
 
                 console.log('number of steps to escape the maze:', maze(input));
+            });
+        })
+    })
+
+    describe('Maze challenge 2', () => {
+        it('should return number of steps to escape the maze', () => {
+            expect(maze2('0 3 0 1 -3')).to.equal(10)
+        })
+
+        it('should count number of moves to escape the maze', () => {
+            fs.readFile('./5/maze.txt', 'utf8', (err, contents) => {
+                const input = contents
+                    .split('\n')
+                    .filter(line => line !== '')
+                    .join(' ')
+
+                console.log('number of steps to escape the maze:', maze2(input));
             });
         })
     })
