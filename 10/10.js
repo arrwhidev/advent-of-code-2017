d4c5fcbd25e073a0a6bf3ae8970660452de1c562
arran <arrwhidev@gmail.com>
2017-12-23 16:26:48 +0000

    Day 10 part 2

--- 10/10.js
+++ 10/10.js
@@ -6,6 +6,12 @@ Array.prototype.cyclicSlice = function(start, size) {
     return slice
 }
 
+function toAsciiCodes(str) {
+    return str
+      .split('')
+      .map(c => c.charCodeAt(0))
+}
+
 function hash(size, input) {
     const seq = resequence(size, input)
     return seq[0] * seq[1]
@@ -38,6 +44,50 @@ function generateSeq(size) {
     return arr
 }
 
+function knotHash(size, input) {
+    const asciiCodes = toAsciiCodes(input)
+    asciiCodes.push(...[17, 31, 73, 47, 23])
+
+    let arr = generateSeq(size)
+    let curr = 0
+    let skip = 0
+
+    for (let i = 0; i < 64; i++) {
+        asciiCodes
+            .map(i => parseInt(i))
+            .forEach(len => {
+                let tmp = curr
+                arr.cyclicSlice(curr, len).reverse().forEach(num => {
+                    arr[tmp] = num
+                    tmp = (tmp + 1) % size
+                })
+
+                curr = (curr + len + skip) % size
+                skip++
+            })
+    }
+
+    return denseHash(16, arr).reduce(toHex, '')
+}
+
+function denseHash(size, arr) {
+    const dense = []
+    while(arr.length > 0) {
+        const xor = arr
+            .splice(0, size)
+            .reduce((total, num) => (!total) ? num : total ^ num)
+        dense.push(xor)
+    }
+    return dense
+}
+
+const toHex = (hex, num) => {
+    const h = num.toString(16)
+    return (h.length === 1)
+        ? `${hex}0${h}`
+        : `${hex}${h}`
+}
+
 module.exports = {
-    hash, resequence
+    hash, resequence, toAsciiCodes, knotHash, denseHash, toHex
 }
--- 10/10.spec.js
+++ 10/10.spec.js
@@ -1,8 +1,11 @@
 const { expect } = require('chai')
 const fs = require('fs')
 const {
+    toAsciiCodes,
     resequence,
-    hash
+    hash,
+    denseHash,
+    knotHash
 } = require('./10')
 
 describe('Day 10', () => {
@@ -22,6 +25,54 @@ describe('Day 10', () => {
         it('should compute hash for real input', () => {
             console.log('The hash is',
                 hash(256, '157,222,1,2,177,254,0,228,159,140,249,187,255,51,76,30')
+            )
+        })
+    })
+
+    describe('Challenge 2', () => {
+        it('should convert to ascii codes', () => {
+            expect(
+                toAsciiCodes('1,2,3')
+            ).to.deep.equal([
+                49,44,50,44,51
+            ])
+        })
+
+        it('should reduce the sparse hash into a dense hash', () => {
+            expect(
+                denseHash(4, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
+            ).to.deep.equal([
+                4, 12, 4, 28
+            ])
+
+            expect(
+                denseHash(16, [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22])
+            ).to.deep.equal([
+                64
+            ])
+        })
+
+        it('should produce a hash', () => {
+            expect(
+                knotHash(256, '1,2,3')
+            ).to.equal('3efbe78a8d82f29979031a4aa0b16a9d')
+
+            expect(
+                knotHash(256, 'AoC 2017')
+            ).to.equal('33efeb34ea91902bb2f59c9920caa6cd')
+
+            expect(
+                knotHash(256, '1,2,3')
+            ).to.equal('3efbe78a8d82f29979031a4aa0b16a9d')
+
+            expect(
+                knotHash(256, '1,2,4')
+            ).to.equal('63960835bcdc130f0b66d7ff4f6a5a8e')
+        })
+
+        it('should compute hash for real input', () => {
+            console.log('The knot hash is',
+                knotHash(256, '157,222,1,2,177,254,0,228,159,140,249,187,255,51,76,30')
             )
         })
     })
