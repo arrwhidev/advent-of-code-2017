efeb3dab04d67ca92a2faa049a3db64d0a8c22ef
arran <arrwhidev@gmail.com>
2017-12-27 22:09:37 +0000

    Day 13 part 2. Rewrote this several times to be performant enough to compute, runs in ~3 seconds on my machine now.

--- 13/13.js
+++ 13/13.js
@@ -1,50 +1,37 @@
 function parseInput(input) {
-    return input
+    return input.trim()
         .split('\n')
-        .filter(l => l !== '')
         .map(l => {
-            const [depth, range] = l.split(': ')
-            return { depth: parseInt(depth), range: parseInt(range) }
-        });
+            const [depth, range] = l.split(': ').map(Number)
+            return { depth, range }
+        })
 }
 
 function generateLayers(input) {
-    const lines = parseInput(input)
-    const maxDepth = Math.max(...lines.map(l => l.depth))
-    const layers = new Array(maxDepth)
-    for (let d = 0; d < maxDepth; d++) {
-        const { range = 0 } = lines.find(l => l.depth === d) || {}
-        layers[d] = { range }
-    }
-    return layers
+    return parseInput(input)
+        .map(({ depth, range }) => {
+            return { depth, range, offset: (range - 1) * 2 }
+        })
 }
 
-class Scanner {
-    constructor(layers) {
-        layers.forEach(l => {
-            l.scannerPos = 0
-            l.scannerMode = 'inc'
-        })
-    }
-
-    inc(layers) {
-        layers.filter(l => l.range !== 0).forEach(l => {
-            if (l.scannerMode === 'inc' && l.scannerPos === (l.range - 1)) l.scannerMode = 'dec';
-            if (l.scannerMode === 'dec' && l.scannerPos === 0) l.scannerMode = 'inc'
-            l.scannerPos = (l.scannerMode === 'inc') ? l.scannerPos + 1 : l.scannerPos - 1
-        })
-    }
-}
-
-function traverseFirewall(layers) {
-    const scanner = new Scanner(layers)
-    return layers.reduce((severity, layer, i) => {
-        if (layer.scannerPos === 0) {
-            severity += (i * layer.range)
+function traverseFirewall(layers, delay = 0) {
+    let severity = 0
+    layers.forEach(({ depth, range, offset }) => {
+        if ((depth + delay) % offset === 0) {
+            severity += (depth + delay) * range
         }
-        scanner.inc(layers)
-        return severity
-    }, 0);
+    })
+    return severity
 }
 
-module.exports = { generateLayers, traverseFirewall }
+function findFastestPath(layers) {
+    let delay = 0
+    while (true) {
+        if (traverseFirewall(layers, delay) === 0) {
+            return delay
+        }
+        delay++
+    }
+}
+
+module.exports = { generateLayers, traverseFirewall, findFastestPath }
--- 13/13.spec.js
+++ 13/13.spec.js
@@ -2,31 +2,49 @@ const { expect } = require('chai')
 const fs = require('fs')
 const {
     generateLayers,
-    traverseFirewall
+    traverseFirewall,
+    findFastestPath
 } = require('./13')
 
 describe('Day 13', () => {
     describe('Challenge 1 ', () => {
         it('should return severity', () => {
             const layers = [
-                { range: 3 },
-                { range: 2 },
-                { range: 0 },
-                { range: 0 },
-                { range: 4 },
-                { range: 0 },
-                { range: 4 }
+                { depth: 0, range: 3, offset: 4 },
+                { depth: 1, range: 2, offset: 2 },
+                { depth: 4, range: 4, offset: 6 },
+                { depth: 6, range: 4, offset: 6 }
             ]
             expect(traverseFirewall(layers)).to.equal(24)
         })
 
         it('should return severity for real input', (done) => {
             fs.readFile('./13/input.txt', 'utf8', (err, contents) => {
-                const layers = generateLayers(contents);
-                console.log(layers);
                 expect(
-                    traverseFirewall(layers)
+                    traverseFirewall(generateLayers(contents))
                 ).to.equal(632)
+                done()
+            })
+        })
+    })
+
+    describe('Challenge 2 ', () => {
+        it('should find fastest path through the firewall without getting caught', () => {
+            const layers = [
+                { depth: 0, range: 3, offset: 4 },
+                { depth: 1, range: 2, offset: 2 },
+                { depth: 4, range: 4, offset: 6 },
+                { depth: 6, range: 4, offset: 6 }
+            ]
+            expect(findFastestPath(layers)).to.equal(10)
+        })
+
+        it('should return fastest path for real input', function(done) {
+            this.timeout(0) // disable timeout, this takes a few seconds to compute
+            fs.readFile('./13/input.txt', 'utf8', (err, contents) => {
+                expect(
+                    findFastestPath(generateLayers(contents))
+                ).to.equal(3849742)
                 done()
             })
         })
